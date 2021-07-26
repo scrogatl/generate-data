9977e842867b05c1ac78ba9cf3aa0359fa0e7cc9
scrogatl <scott@scottandrewrogers.com>
2021-07-26 10:00:30 -0400

    Initial import

---
+++ gen_release_count.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+#### Functions ###
+display_usage() { 
+	echo -e "Usage: ./gen_metric.sh [metric.name] [metric value]"
+} 
+
+# Check params
+#if [  $# -lt 1 ] 
+#then 
+#	display_usage
+#	exit 1
+#fi 
+
+if [ -z "$1" ]
+then 
+	METRIC=cs.releases
+else 
+	VALUE=$1
+fi
+
+if [ -z "$2" ]
+then 
+	VALUE=$((1 + $RANDOM % 10))
+else 
+	VALUE=$2
+fi
+METRIC=cs.releases
+TOKEN=<API KEY HERE>
+URL=https://longboard.wavefront.com/report
+TYPES=("Bug Fix" "New Feature")
+selectedType=${TYPES[$RANDOM % 2]}
+date
+echo "Sending $METRIC $VALUE $selectedType" 
+curl -H "Authorization: Bearer $TOKEN" --data "$METRIC $VALUE source=deb-docker-host type=\"$selectedType\" " $URL
+
+
+
---
+++ gen_ticket_close_time.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+#### Functions ###
+display_usage() { 
+	echo -e "Usage: ./gen_metric.sh [metric.name] [metric value]"
+} 
+
+# Check params
+#if [  $# -lt 1 ] 
+#then 
+#	display_usage
+#	exit 1
+#fi 
+
+if [ -z "$1" ]
+then 
+	METRIC=cs.tickets.closetime
+else 
+	VALUE=$1
+fi
+
+
+if [ -z "$2" ]
+then 
+	VALUE=$((60000 + $RANDOM % 3600000))
+else 
+	VALUE=$2
+fi
+TOKEN=<API KEY HERE>
+URL=https://longboard.wavefront.com/report
+
+date
+echo "Sending $METRIC $VALUE" 
+curl -H "Authorization: Bearer $TOKEN" --data "$METRIC $VALUE source=deb-docker-host" $URL
+
+
+
